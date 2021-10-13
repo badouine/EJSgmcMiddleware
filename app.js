68c3e67a4c3fc4be9f63807fe25d3cbdcef139d8
badouine <nzbadouine@gmail.com>
2021-10-13 15:52:02 +0000

    style

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+// parametre setup du moteur de vue
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -22,18 +22,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+// 404 page non trouve
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// erreur chargement
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // seul les message d'erreurs
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // rendue de la page
   res.status(err.status || 500);
   res.render('error');
 });
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* Obtenir list de customisation des services. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
